091b6a9e954137d7ecd6dd174ef6bc39c1a00ce5
ClaytonNighthawk <nighthawk1029tv@gmail.com>
2016-10-10 23:38:54 -0700

    node, queue, stack finished but not tested

---
+++ Lab0/src/HW0.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class HW0{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		int numProblems = scanner.nextInt();
+		for(int i = 0; i < numProblems; ++i){
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			int g = gcd(a,b);
+			int l = lcm(a,b);
+			System.out.println(g + " " + l);
+		}
+		scanner.close();
+	}
+
+	public static int gcd(int a, int b){
+	// Find the greatest common divisor of a and b
+	// Hint: Use Euclid's Algorithm
+		if (b == 0){
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+
+	public static int lcm(int a, int b){
+	// Find the least common multiple of a and b
+	// Hint: Use the gcd of a and b
+		return (a * b) / gcd(a,b);
+	}
+}
---
+++ Lab1/TestContents.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TestContents{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        Queue<Character> queue = new Queue<Character>();
+        Stack<Character> stack = new Stack<Character>();
+        while (scan.hasNext()){
+            String input = scan.nextLine();
+            for(int i = 0; i < input.length(); i++){
+                queue.enqueue(input.charAt(i));
+                stack.push(input.charAt(i));
+            }
+            stack.printStack();
+            queue.printQueue();
+        }
+        scan.close();
+    }
+}
